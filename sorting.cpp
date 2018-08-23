f1de369a556414c2bf6a699d3973ab97de9cfc6e
benjamin <yuu750110@gmail.com>
2018-08-23 10:47:25 +0800

    add sorting algos.

--- sorting.cpp
+++ sorting.cpp
@@ -33,6 +33,43 @@ public:
         }
     };
 
+    void bubbleSort(int* a, int size)
+    {
+        for (int i=1; i<size; i++)
+            for (int j=0; j<i; j++)
+                if (a[j]>a[i])
+                    swap(a[j], a[i]);
+    };
+
+    void insertionSort(int* a, int n)
+    {
+        for (int i=1; i<n; i++)
+        {
+            int key = a[i];
+            int j = i-1;
+            while (j>=0 && a[j]>key)
+            {
+                a[j+1] = a[j];
+                j--;
+            }
+            a[j+1] = key;
+        }
+    };
+
+    void selectionSort(int* a, int n)
+    {
+        for (int i=0; i<n; i++)
+        {
+            int min = i;
+            for (int j=i+1; j<n; j++)
+            {
+                if (a[min]>a[j])
+                    min = j;
+            }
+            swap(a[i], a[min]);
+        }
+    };
+
 private:
     void swap(int& a, int& b)
     {
@@ -49,7 +86,10 @@ int main(void)
     int size = sizeof(a)/sizeof(a[0]);
 
     sorting s;
-    s.qsort(a, 0, size-1);
+    //s.qsort(a, 0, size-1);
+    //s.bubbleSort(a, size);
+    //s.insertionSort(a, size);
+    s.selectionSort(a, size);
 
     cout << "size = " << size <<endl;
     for (int i=0; i<size; i++)
